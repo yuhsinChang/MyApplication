fb4ebf57446f9b91218e861065ec5a6c0978b501
yuhsinChang <changbrian0615@outlook.com>
2017-05-25 15:05:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cjcu/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.cjcu.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    String[] Fruit={"Apple","Kiwi","Bananananananananananananananana","Pineapple"};
+    private ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.list);
+        ArrayAdapter adapter =new ArrayAdapter(this,android.R.layout.simple_list_item_1,Fruit);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, list.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
